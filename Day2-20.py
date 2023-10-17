df0168074e6d3febd7aba1fdd501324ad197e439
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-10-17 14:46:21 +0530

    Update Day2-20.py

--- Day2-20.py
+++ Day2-20.py
@@ -14,3 +14,6 @@ index_list = df[df['Text'].str.find(substring) != -1].index
 print("Indices of rows where the substring '{}' is found:".format(substring))
 for index in index_list:
     print(index)
+
+Indices of rows where the substring 'ban' is found:
+1

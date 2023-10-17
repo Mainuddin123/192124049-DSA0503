fb89ac9e2ebb71cf7751f6b45941166ef27eff1d
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-10-17 08:31:39 +0530

    Update Day1-p1.py

--- Day1-p1.py
+++ Day1-p1.py
@@ -18,3 +18,32 @@ df = pd.DataFrame(data)
 distinct_department_ids = df['DEPARTMENT_ID'].unique()
 
 print(distinct_department_ids)
+
+DEPARTMENT_ID
+0              10
+1              20
+2              30
+3              40
+4              50
+5              60
+6              70
+7              80
+8              90
+9             100
+10            110
+11            120
+12            130
+13            140
+14            150
+15            160
+16            170
+17            180
+18            190
+19            200
+20            210
+21            220
+22            230
+23            240
+24            250
+25            260
+26            270

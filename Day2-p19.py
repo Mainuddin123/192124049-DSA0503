d8e29801043dd112972e8c1dfd9205eb71447703
Sk Khaja Mainuddin <112795691+Mainuddin123@users.noreply.github.com>
2023-10-17 13:35:07 +0530

    Update Day2-p19.py

--- Day2-p19.py
+++ Day2-p19.py
@@ -7,3 +7,22 @@ print('\nNumber of rows: ',w_a_con.shape[0])
 print('\nNumber of column: ',w_a_con.shape[1])
 print("\nExtract Column Names:")
 print(w_a_con.columns)
+
+World alcohol consumption sample data:
+   Year           WHO region  ...      Types DisplayValue
+0  1986   Western region      ...   Wine            0.000
+1  1986  Americas             ...   Other           0.500
+2  1985              Africa   ...    Wine           1.620
+3  1986       Americans       ...    Beer           4.427
+4  1987    Americans          ...    Beer           1.980
+
+[5 rows x 5 columns]
+
+Shape of the dataframe:  (5, 5)
+
+Number of rows:  5
+
+Number of column:  5
+
+Extract Column Names:
+Index(['Year', 'WHO region', ' Country Bevarage', 'Types', 'DisplayValue'], dtype='object')
